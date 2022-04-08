10dab8bee0c1abbc6f7a0f00d46bc612b56982a1
MWERO ABDALLA <mwerothewebmaker@gmail.com>
2022-04-08 23:39:09 +0000

    Added 7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -10,12 +10,12 @@
 int main(void)
 {
 /*Declarations*/
-        char letter;
+	char letter;
 
-        for (letter = 'z'; letter >= 'a'; letter--)
-        {
-        putchar(letter);
-        }
-        putchar('\n');
-        return (0);
+	for (letter = 'z'; letter >= 'a'; letter--)
+	{
+	putchar(letter);
+	}
+	putchar('\n');
+	return (0);
 }
