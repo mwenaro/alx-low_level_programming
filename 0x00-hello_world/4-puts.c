ba7f08451200ab46f4e10f6706b7e59db0563405
MWERO ABDALLA <mwerothewebmaker@gmail.com>
2022-04-08 01:18:08 +0000

    Added 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - Fxn beiings, i'ts the
+*enrty poin for all c-programs
+*
+*
+* Return: returns zoro (int)
+*
+*/
+
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle, \n");
+
+return (0);
+}
