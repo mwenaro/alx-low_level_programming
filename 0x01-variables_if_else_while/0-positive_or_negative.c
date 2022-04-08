082f79e5bf2e155167d850c2e3a082ffd41392f1
MWERO ABDALLA <mwerothewebmaker@gmail.com>
2022-04-08 19:20:34 +0000

    Added 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,10 +5,10 @@
 /* betty style doc for function main goes there */
 
 /**
- * main - Prints is +v, is -ve or is 0
- * 
+ * main - prints a string on the console
  *
- * Return: 0 is all goes well.
+ * Return: 0 when all goes well
+ *
  */
 
 int main(void)
@@ -19,17 +19,12 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 	if (n > 0)
-
-	printf("%d is positive\n",n);
-
+		printf("%d is positive\n",n);
 	else if (n == 0)
-		
-	printf("%d is zero\n",n);
+		printf("%d is zero\n",n);
+	else 
+		printf("%d is negative\n",n);
 
-	else if (n < 0) 
-	
-	printf("%d is negative\n",n); 
-	
 
 	return (0);
 }
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,1 +1,2 @@
-0x01-variables_if_else_while
+### My Readme file for 0x01-variables_if_else_while
+
