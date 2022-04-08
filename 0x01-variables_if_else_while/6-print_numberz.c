fabe0c7848cc1278e40d0878cfb036045acf6444
MWERO ABDALLA <mwerothewebmaker@gmail.com>
2022-04-08 21:58:33 +0000

    Added 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+/**
+ * main - prints the digits of the alphabets
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+/*Declarations*/
+	int digit;
+
+	for (digit = 0; digit < 10; digit++)
+	{
+	putchar(digit);
+	}
+	putchar('\n');
+	return (0);
+}
