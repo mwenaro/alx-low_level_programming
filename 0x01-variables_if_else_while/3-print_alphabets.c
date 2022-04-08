dd2a5fd2a5c9c894337ff04818d5b2912355c7f0
MWERO ABDALLA <mwerothewebmaker@gmail.com>
2022-04-08 21:08:43 +0000

    Added 3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+
+/**
+ * main - prints the letters of the alphabets
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+/*Declarations*/
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+	putchar(letter);
+	}
+	for (letter = 'A'; letter <= 'Z'; letter++)
+	{
+	putchar(letter);
+	}
+	return (0);
+}
