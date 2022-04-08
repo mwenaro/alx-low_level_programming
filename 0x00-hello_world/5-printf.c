93efb6766226b0aa845bb00624e0b7df8854d650
MWERO ABDALLA <mwerothewebmaker@gmail.com>
2022-04-08 01:31:41 +0000

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - Fxn beiings, i'ts the
+*enrty poin for all c-programs
+*
+*
+* Return: returns zoro (int)
+*
+*/
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art\n");
+
+return (0);
+}
