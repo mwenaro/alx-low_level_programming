bd313ddc1e6da9e34ca01351ca9a16ead4d063a7
MWERO ABDALLA <mwerothewebmaker@gmail.com>
2022-04-09 00:16:19 +0000

    Added 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+
+/**
+ * main - prints the digits of the alphabets
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+/*Declarations*/
+	int digit;
+
+	for (digit = 48; digit < 58; digit++)
+	{
+	/*putchar(digit);*/
+	putchar(digit);
+
+		if (digit != 57)
+		{
+  		putchar(',');
+		putchar(' ');
+		}
+
+	}
+	putchar('\n');
+	return (0);
+}
