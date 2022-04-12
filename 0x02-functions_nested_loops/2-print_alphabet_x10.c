7579cb49c133ef4e5cc98824cbfc04ffa42ac856
MWERO ABDALLA <mwerothewebmaker@gmail.com>
2022-04-12 21:24:35 +0000

    Upadeted 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints all the alphabets in lower case 10 times
+ *
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	char mychar;
+	int n;
+
+	n = 0;
+	while (n < 10)
+	{
+		for (mychar = 'a'; mychar <= 'z'; mychar++)
+			_putchar(mychar);
+
+		_putchar('\n');
+	}
+}
+
