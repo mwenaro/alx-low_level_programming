654c551b99ded719acfc040e4f68e1570b117a23
MWERO ABDALLA <mwerothewebmaker@gmail.com>
2022-04-13 18:56:55 +0000

    Updated files again

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,18 +1,25 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
- * main - prints a string on the console
+ * positive_or_negative - Entry point
  *
- * Return: void
- *
+ * Return: Always 0 (Success)
  */
-
- void positive_or_negative(int i)
+int positive_or_negative(void)
 {
-	if (i > 0)
-	printf("%d is positive\n", i);
-	else if (i == 0)
-	printf("%d is zero\n", i);
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
 	else
-	printf("%d is negative\n", i);
+		printf("%d is negative\n", n);
+
+	return (0);
 }
