fa064381ae74eb1e011b55e0127abe047334e8e0
MWERO ABDALLA <mwerothewebmaker@gmail.com>
2022-04-13 17:09:48 +0000

    Updated 0-main.c, main.h

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,28 +1,18 @@
-#include <stdlib.h>
-#include <time.h>
-/* more headers goes there */
-#include <stdio.h>
-/* betty style doc for function main goes there */
+#include "main.h"
 
 /**
  * main - prints a string on the console
  *
- * Return: 0 when all goes well
+ * Return: void
  *
  */
 
-int main(void)
+ void positive_or_negative(int i)
 {
-	int n;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	if (n > 0)
-	printf("%d is positive\n", n);
-	else if (n == 0)
-	printf("%d is zero\n", n);
+	if (i > 0)
+	printf("%d is positive\n", i);
+	else if (i == 0)
+	printf("%d is zero\n", i);
 	else
-	printf("%d is negative\n", n);
-	return (0);
+	printf("%d is negative\n", i);
 }
