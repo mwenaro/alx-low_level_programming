cf9b5a72ab1313f3c9822b5fd7f77eba3d3bf3f7
MWERO ABDALLA <mwerothewebmaker@gmail.com>
2022-04-15 02:17:37 +0000

    Updated task 3 files

--- 0x03-debugging/3-main_b.c
+++ 0x03-debugging/3-main_b.c
@@ -17,7 +17,7 @@ int main(void)
 	day = 29;
 	year = 2000;
 	printf("Date: %02d/%02d/%04d\n", month, day, year);
-	day = convert_day(month, day);
-	print_remaining_days(month, day, year);
+	/*day = convert_day(month, day);*/
+	/*print_remaining_days(month, day, year);*/
 	return (0);
 }
