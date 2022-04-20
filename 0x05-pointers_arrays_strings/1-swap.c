ed0995d6a34e5bf55c789a30e9e8105cf17f885b
MWERO ABDALLA <mwerothewebmaker@gmail.com>
2022-04-20 17:08:39 +0000

    added 1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - check the code
+ * @a : 1st ag
+ * @b: @nd ag
+ * Return: Always 0.
+ */
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	temp = *a, *a = *b, *b = temp;
+
+}
+
