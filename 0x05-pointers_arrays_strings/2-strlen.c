2609a86c052473ea868dccbf78ebed7371b12b74
MWERO ABDALLA <mwerothewebmaker@gmail.com>
2022-04-20 18:51:59 +0000

    Added 2-strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - check the code
+ * @s: The str whose length is to be calculated
+ * Return: len of str
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
+
