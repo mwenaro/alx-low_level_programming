d20bd022fae1b69a87c87a6e1d91dd06e3776741
MWERO ABDALLA <mwerothewebmaker@gmail.com>
2022-04-21 20:41:50 +0000

    Added 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - rests the value of n to 98
+ * @s: the str whose is to be returned
+ * Return: str len
+ */
+int _strlen(char *s)
+{
+	return strlen(s);
+}
