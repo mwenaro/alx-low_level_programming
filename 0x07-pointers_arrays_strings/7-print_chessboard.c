80e6e6b245865f11c1ae6e405f52fd8f092973ae
MWERO ABDALLA <mwerothewebmaker@gmail.com>
2022-04-26 23:34:49 +0000

    Added 7-print_chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -10,12 +10,13 @@ void print_chessboard(char (*a)[8])
 	int i, j;
 
 	i = 0;
-	while (i++ < 8)
+	while (i < 8)
 	{
 		j = 0;
-		while (j++ < 8)
-			_putchar(*(*(i + a) + j));
+		while (j < 8)
+			_putchar(*(*(i + a) + j++));
 
 		_putchar('\n');
+		i++;
 	}
 }
