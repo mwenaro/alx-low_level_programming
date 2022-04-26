35628c8fb3f5305895ef50e71f2ebb7a5fbb7a8a
MWERO ABDALLA <mwerothewebmaker@gmail.com>
2022-04-26 23:31:01 +0000

    Added 7-print_chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints chessboard.
+ * @a: matrix
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	i = 0;
+	while (i++ < 8)
+	{
+		j = 0;
+		while (j++ < 8)
+			_putchar(*(*(i + a) + j));
+
+		_putchar('\n');
+	}
+}
