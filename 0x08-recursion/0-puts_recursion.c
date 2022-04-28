0d2f5910d00265031e618cc90be8d57168a9120b
MWERO ABDALLA <mwerothewebmaker@gmail.com>
2022-04-28 22:12:11 +0000

    Added 0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - check the code
+ *@s: s the str to be printed
+ * Return: nothing.
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
