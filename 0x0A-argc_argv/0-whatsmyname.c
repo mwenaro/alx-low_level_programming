37b0256aecd092d456755ec7317c8ae9439cafeb
MWERO ABDALLA <mwerothewebmaker@gmail.com>
2022-05-06 03:53:02 +0000

    Added 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+  *main - Entry poinear
+  *
+  *@argc: Number of comand line arguents
+  *@argv: An array containing str values of rthe command line arguments
+  * Return: 0
+  */
+
+int main(int argc, char **argv)
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
