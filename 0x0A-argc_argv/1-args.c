2158ace295593d4e58834c916b6e830b96b3228a
MWERO ABDALLA <mwerothewebmaker@gmail.com>
2022-05-06 04:23:00 +0000

    Added 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
 /**
-  *main - Entry poinear
-  *
-  *@argc: Number of comand line arguents
-  *@argv: An array containing str values of rthe command line arguments
-  * Return: 0
-  */
+ *main - Entry poinear
+ *
+ *@argc: Number of comand line arguents
+ *@argv: An array containing str values of rthe command line arguments
+ * Return: 0
+ */
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-	(void)argv;
 	if (argc > 0)
-		printf("%di\n", argc - 1);
+		printf("%d\n", argc - 1);
 	return (0);
 }
+
