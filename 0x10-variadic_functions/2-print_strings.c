2e07a92c80807d144e18cb3284f223105b96b9b6
MWERO ABDALLA <mwerothewebmaker@gmail.com>
2022-05-18 02:59:33 +0000

    Added 2-print...

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -22,7 +22,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("%s", val);
 		else
 			printf("%p", val);
-		if (separator != NULL && s < n - 1)
+		if (separator != NULL && i < n - 1)
 			printf("%s", separator);
 	}
 	printf("\n");
