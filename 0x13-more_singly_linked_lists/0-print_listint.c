6bc2ae8f6ebe182af05ef4a137cff51892092f8a
MWERO ABDALLA <mwerothewebmaker@gmail.com>
2022-05-31 02:32:48 +0000

    Added task 1

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_listint - prints all the elements of a listint_t list.
+ * @h: Pointer to a list.
+ * Return: num of items
+ **/
+
+size_t print_listint(const listint_t *h)
+{
+	const listint_t *head;
+	unsigned int i = 0;
+
+	head = h;
+	while (tp)
+	{
+		printf("%d\n", tp->n);
+		i++;
+		head = head->next;
+	}
+	return (i);
+}
